9fb0437e2e6822d50d8990ec2fd94c0b31428ef7
Suveetha <suveethamanimuthu@gmail.com>
2023-04-23 22:04:41 +0530

    Update Hello.js

--- Hello.js
+++ Hello.js
@@ -1,1 +1,9 @@
-window.alert("Helloo world!!")
+function hello(){
+  window.alert("Hello World!!");
+  console.log("hello world is alerted successfully");
+}
+
+/*
+JAVASCRIPT  is a language which helps in making the front-end more active;
+
+*/
